2970b40a2e2f2e017c94daaf6d98de8c8f237ba9
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-15 15:16:15 +0530

    code part 4.2

--- app/src/main/java/com/example/firebasedemoproject/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/firebasedemoproject/ui/profile/ProfileFragment.kt
@@ -1,23 +1,83 @@
 package com.example.firebasedemoproject.ui.profile
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.firebasedemoproject.R
+import com.example.firebasedemoproject.databinding.FragmentHomeBinding
+import com.example.firebasedemoproject.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
+    val REQUEST_IMAGE_GET = 1
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        auth = Firebase.auth
+        db = Firebase.firestore
+        return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fab.setOnClickListener { }
+        binding.imageProfile.setOnClickListener {
+            selectImage()
+        }
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_GET)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_GET) {
+            if (resultCode == Activity.RESULT_OK) {
+                val thumbnail: Bitmap? = data?.getParcelableExtra("data")
+                val fullPhotoUri: Uri? = data?.data
+                Glide.with(requireActivity()).load(fullPhotoUri).into(binding.imageProfile)
+            } else {
+                // todo handle cancel logic
+            }
+        }
+    }
+
+    class SelectorDialogFragment : DialogFragment() {
+        // todo add code later
+    }
 }
